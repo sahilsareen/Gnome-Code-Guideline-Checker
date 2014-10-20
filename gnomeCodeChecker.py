fa9548804a340d878f500e38a9664863db6c28d7
Sahil Sareen <sahil.sareen@hotmail.com>
2014-10-21 00:17:30 +0530

    Check if you comply with the gnome guidelines
    Most generally committed  errors checked: Line width, lines with
    trailing whitespace, and other whitespace checks around brackets,
    NOT checking the block structures(like if-elses), as editor does that
    for you(Assuming you don’t mess around :P).

---
+++ gnomeCodeChecker.py
@@ -0,0 +1,58 @@
+# script to check if any gnome coding guidelines are violated
+import sys
+import re
+
+def find(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch]
+
+# Read the file
+with open( sys.argv[1] ) as f:
+    lines = f.readlines()
+
+# Line Width
+for lineNum in xrange( 0, len( lines ) ):
+    if len( lines[ lineNum ] ) > 120:
+        print "Line ", lineNum+1, " is greater than 120 charecters"
+    
+# Lines with trailing white-space
+for lineNum in xrange( 0, len( lines ) ):
+    if lines[ lineNum ].endswith(' '):
+        print "Line ", lineNum+1, " has trailing whitespace"
+
+# Single whitespace before "{"
+for lineNum in xrange( 0, len( lines ) ):
+    indexes = find( lines[ lineNum ], '{' )
+    for index in indexes:
+        if index > 1:
+            if lines[ lineNum ][ index - 1 ] != ' ' and lines[ lineNum ][ index - 1 ] != '_':
+                print "Line ", lineNum+1, " missing whitespace before {"
+            elif lines[ lineNum ][ index - 2 ] == ' ' and lines[ lineNum ][ index - 1 ] != '_' \
+                    and not ( re.findall( "\s*{", lines[ lineNum ] ) != [] and lines[ lineNum ][ 0 ] in [ '\t', ' ' ] ):
+                print "Line ", lineNum+1, " multiple whitespace before {"
+
+# Single whitespace before "("
+for lineNum in xrange( 0, len( lines ) ):
+    indexes = find( lines[ lineNum ], '(' )
+    for index in indexes:
+        if index > 1:
+            if lines[ lineNum ][ index - 1 ] != ' ' and lines[ lineNum ][ index - 1 ] not in [ '_', '(', '&', '*', '\t' ]:
+                print "Line ", lineNum+1, " missing whitespace before ("
+            elif lines[ lineNum ][ index - 2 ] == ' ' and lines[ lineNum ][ index - 1 ] not in [ '_', '(' ] \
+                    and not ( lines[ lineNum ].startswith(" *") or lines[ lineNum ].startswith("#") or
+                              ( re.findall( "\s*\(", lines[ lineNum ] ) != [] and lines[ lineNum ][ 0 ] in [ '\t', ' ' ] ) ):
+                print "Line ", lineNum+1, " multiple whitespace before ("
+               
+# No whitespce between round brackets "(xyz)"
+for lineNum in xrange( 0, len( lines ) ):
+    indexes = find( lines[ lineNum ], '(' )
+    for index in indexes:
+        if index > 1:
+            if lines[ lineNum ][ index + 1 ] == ' ':
+                print "Line ", lineNum+1, " has whitespace after ("
+
+for lineNum in xrange( 0, len( lines ) ):
+    indexes = find( lines[ lineNum ], ')' )
+    for index in indexes:
+        if index > 1:
+            if lines[ lineNum ][ index - 1 ] == ' ':
+                print "Line ", lineNum+1, " has whitespace before )"
